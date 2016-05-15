38ffaa99669738ef45bdd3d7000f222809973466
Eric Duong <eduong117@gmail.com>
2016-05-14 22:42:23 -0700

    in an array print most consecutive numbers

---
+++ printMostConsecNum.rb
@@ -0,0 +1,18 @@
+count = 0 
+count2 = 0
+i = 0
+arr=[]
+puts "enter an array of numbers"
+def modeArray(arr)
+	until i == length.arr
+		if arr[i] == arr[i+1]
+			count += 1
+			i+=1
+		else
+			if count > count2
+			count = count2
+			count = 0	
+		end
+	end
+	puts count2 + 1 
+end
