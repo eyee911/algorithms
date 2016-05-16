041fc3eb368732d87170fc1890dcd6981b3b709b
Eric Duong <eduong117@gmail.com>
2016-05-15 23:52:30 -0700

    added function to populate an array

--- printMostConsecNum.rb
+++ printMostConsecNum.rb
@@ -2,7 +2,16 @@ count = 0
 count2 = 0
 i = 0
 arr=[]
-puts "enter an array of numbers"
+
+def popArray(int)
+	puts "enter a number"
+	if num > 0 || num < 0
+		arr.push(num)
+	else 
+		puts "not a number"
+	end
+end
+
 def modeArray(arr)
 	until i == length.arr
 		if arr[i] == arr[i+1]
