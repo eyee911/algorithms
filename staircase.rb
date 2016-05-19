593ae0184b5c91666d1a1fe76445ef501c610ef4
Eric Duong <eduong117@gmail.com>
2016-05-18 22:43:43 -0700

    fixed it and placed into a function

--- staircase.rb
+++ staircase.rb
@@ -1,6 +1,19 @@
-puts "enter height of staircase"
-height = gets.chomp.to_i
-i = 0
-j = 0
-until i == height
-	
+def consArray(array)
+	i = 0
+	count = 0
+	count2 = 0
+	while i < array.length - 1
+	if array[i] == array[i+1]
+		count+=1
+	else 
+		
+		count = 0
+		count2 = count
+	end
+	i+=1
+end
+puts count2
+end
+
+arr = [1,1,1,2,2]
+consArray(arr)
