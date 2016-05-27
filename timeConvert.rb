e54ad5297967b89158b67fc09dbd99ef148e89ce
Eric Duong <eduong117@gmail.com>
2016-05-27 13:56:22 -0700

    fixed just need input validation

--- timeConvert.rb
+++ timeConvert.rb
@@ -1,10 +1,52 @@
-def timeConvert()
-	ericTime=Array.new
-	time = gets.chomp
-	ericTime.push(time.slice(/.../))
-	puts ericTime[0]
- 
+def timeConvert(time)
+=begin	time.each_char do |x|
+=end		if x == 'P'
+		#if time.scan(/P/) 
+			ericTime=time.split(/[:\D]/,4)
+			hours = ericTime[0]
+			newHours = hours.to_i
+		
+			if newHours < 12
+				milHours = newHours + 12
+			else
+				milHours = newHours - 12
+				if milHours == 0
+					milHours = "00"
+				end
+			end
+		
+		ericTime[0] = milHours
+		timePM =  "#{ericTime[0]}:#{ericTime[1]}:#{ericTime[2]}"
+
+		else 
+			ericTime=time.split(/[:\D]/,4)
+			timeAM = "#{ericTime[0]}:#{ericTime[1]}:#{ericTime[2]}"
+		end
+	#end
+	
+time.each_char do |x|
+	if x == 'P'
+		puts timePM 
+	else 
+		puts timeAM
+	end
 end
-timeConvert()
+
 	
 	
+	#ericTime=time.split(/[:\D]/,4)
+	#puts ericTime[0..3]
+	#puts ericTime[0] + ":" + ericTime[1] + ":" + ericTime[2]
+#end
+timeConvert('1:34:54PM')
+
+string = "cat"
+
+string.each_char do |x|
+	if x == 'a'
+	puts x
+	end
+end
+
+#time = "12:34:54PM"
+#puts time.scan(/\D/)
